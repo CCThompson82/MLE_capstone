814d1bfb5ff067cf322fca36f45c8ee271720740
Christopher Thompson <christophercthompson82@gmail.com>
2016-07-27 19:08:22 +0100

    Add ylable to final graph

--- Support Files/final_graph.py
+++ Support Files/final_graph.py
@@ -21,3 +21,5 @@ A.plot(range(0,metric_stat.shape[0],1),
           color = 'black', label = 'LogLoss score',
           marker= 'o')
 A.legend(loc=2)
+A.set_ylabel('Metric Score', fontsize=16)
+fig.suptitle('Summary of project test set performance', fontsize = 20)
