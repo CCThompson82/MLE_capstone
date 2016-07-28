ed84eb8e49f75a4660d24b87f42aa1320984c993
Christopher Thompson <christophercthompson82@gmail.com>
2016-07-28 09:19:48 +0100

    Change Training set size to 80%

--- Support Files/capstone_performance.py
+++ Support Files/capstone_performance.py
@@ -9,3 +9,9 @@ metric_stat = metric_stat.append(pd.DataFrame({'F2': fbeta_score(y_test, clf_LR.
 print('\nLog Loss: ',log_loss(y_test.replace({'n0':0, 'n1':1}),
           clf_LR.predict_proba(X_test)[:,1]))
 print("\n\n",metric_stat)
+
+training_stat = training_stat.append(pd.DataFrame({'F2': fbeta_score(y_train, clf_LR.predict(X_train), pos_label='n1',beta=2),
+                                 'MCC': matthews_corrcoef(y_train, clf_LR.predict(X_train)),
+                                 'LogLoss':log_loss(y_train.replace({'n0':0, 'n1':1}),
+                                           clf_LR.predict_proba(X_train)[:,1])}, index = [run_name]),
+                                           ignore_index=False)
--- Support Files/final_graph.py
+++ Support Files/final_graph.py
@@ -1,5 +1,6 @@
-fig = plt.figure(figsize=(8,8))
-A = fig.add_subplot(1,1,1)
+fig = plt.figure(figsize=(12,6))
+A = fig.add_subplot(1,2,1)
+B = fig.add_subplot(1,2,2)
 ya = 0.2
 yb = 0.9
 A.set_ylim(ya,yb)
@@ -7,20 +8,43 @@ A.set_xlim(-0.5,4.5)
 A.xaxis.set_major_formatter(plt.NullFormatter())
 for i,x in enumerate(metric_stat.index) :
     A.text(i-.25,ya-((yb-ya)/20),x, rotation = -45, fontsize=14)
-#A.text([0,1,2,3], [1,1,1,1], [metric_stat.index])
+
+B.set_ylim(ya,yb)
+B.set_xlim(-0.5,4.5)
+B.xaxis.set_major_formatter(plt.NullFormatter())
+for i,x in enumerate(metric_stat.index) :
+    B.text(i-.25,ya-((yb-ya)/20),x, rotation = -45, fontsize=14)
+
+
 A.plot(range(0,metric_stat.shape[0],1),
           metric_stat['F2'],
-          color = 'blue', label = 'F2 score',
+          color = 'blue', label = 'F2 test score',
           marker= 'o')
+A.plot(range(0,training_stat.shape[0],1),
+          training_stat['F2'],
+          color = 'blue',
+          label = 'F2 training score',
+          linestyle ='--')
 A.plot(range(0,metric_stat.shape[0],1),
           metric_stat['MCC'],
           color = 'green', label = 'MCC score',
           marker= 'o')
-A.plot(range(0,metric_stat.shape[0],1),
+A.plot(range(0,training_stat.shape[0],1),
+          training_stat['MCC'],
+          color = 'green',
+          label = 'MCC training score',
+          linestyle ='--')
+B.plot(range(0,metric_stat.shape[0],1),
           metric_stat['LogLoss'],
           color = 'black', label = 'LogLoss score',
           marker= 'o')
+B.plot(range(0,training_stat.shape[0],1),
+          training_stat['LogLoss'],
+          color = 'black',
+          label = 'LogLoss training score',
+          linestyle ='--')
 A.legend(loc=2)
+B.legend(loc=2)
 A.set_ylabel('Metric Score', fontsize=16)
 fig.suptitle('Summary of project test set performance', fontsize = 20)
 plt.show()
