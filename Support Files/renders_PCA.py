e76e3ab6b30d825c4f12401eeb6e9b4991a0eab0
Christopher Thompson <christophercthompson82@gmail.com>
2016-08-02 10:07:04 +0100

    Change dpi on unreadable figures

--- Support Files/renders_PCA.py
+++ Support Files/renders_PCA.py
@@ -22,16 +22,16 @@ def pca_results(good_data, pca):
 
 	# Plot the feature weights as a function of the components
 	components.plot(ax = ax, kind = 'bar', legend=False);
-	ax.set_ylabel("Feature Weights")
+	ax.set_ylabel("Feature Weights", fontsize=16)
 	ax.set_ylim(-.5,0.6)
-	ax.set_xticklabels(dimensions, rotation=0)
+	ax.set_xticklabels(dimensions, rotation=0, fontsize=16)
 	patches, labels = ax.get_legend_handles_labels()
-	ax.legend(patches, labels, loc= 1, ncol=5)
+	ax.legend(patches, labels, loc= 'lower center', ncol=10)
 
 
 	# Display the explained variance ratios
 	for i, ev in enumerate(pca.explained_variance_ratio_):
-		ax.text(i-.1, ax.get_ylim()[1] + 0.01, "Explained Variance\n %.4f"%(ev))
+		ax.text(i-.1, ax.get_ylim()[1] + 0.01, "Explained Variance\n %.4f"%(ev), fontsize=16)
 
 	# Return a concatenated DataFrame
 	return pd.concat([variance_ratios, components], axis = 1)
